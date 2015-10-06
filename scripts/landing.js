6f62b363a3d694f4e92452e97acffc87e74a0c7a
winber1 <winber@cox.net>
2015-10-06 15:32:49 -0400

    checkpoint 23 complete: DOM Scripting: Events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,18 +1,35 @@
-var animatePoints = function() 
-{
-    var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
+ 
+var animatePoints = function(points) {
+    
+    var revealPoint = function(j) 
+    {
+        points[j].style.opacity = 1;
+        points[j].style.transform = "scaleX(1) translateY(0)";
+        points[j].style.msTransform = "scaleX(1) translateY(0)";
+        points[j].style.WebkitTransform = "scaleX(1) translateY(0)";   
+    };
                   
     for(var i=0; i< points.length; i++)
-    { 
-         var revealPoint = function(j) 
-        {
-            points[j].style.opacity = 1;
-            points[j].style.transform = "scaleX(1) translateY(0)";
-            points[j].style.msTransform = "scaleX(1) translateY(0)";
-            points[j].style.WebkitTransform = "scaleX(1) translateY(0)";   
-        };
-        
-        revealPoint(i);
-    }
+    { revealPoint(i); }
 };
+
+window.onload = function()
+{
+     // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+     if (window.innerHeight > 950) 
+     { animatePoints(pointsArray); }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener
+    ('scroll', function(event) 
+            { 
+                console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels"); 
+                if (document.body.scrollTop >= scrollDistance) 
+                { animatePoints(pointsArray); }
+            }
+    );
+}
 
