eca6efef75b837dbc48a00c34fe480c4a37c224a
winber1 <winber@cox.net>
2015-10-06 20:30:20 -0400

    assignment 23 complete - DOM Scripting: Events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,18 +1,17 @@
 var pointsArray = document.getElementsByClassName('point');
  
-var animatePoints = function(points) {
-    
-    var revealPoint = function(j) 
-    {
-        points[j].style.opacity = 1;
-        points[j].style.transform = "scaleX(1) translateY(0)";
-        points[j].style.msTransform = "scaleX(1) translateY(0)";
-        points[j].style.WebkitTransform = "scaleX(1) translateY(0)";   
-    };
-                  
-    for(var i=0; i< points.length; i++)
-    { revealPoint(i); }
+var revealPoint = function(point) 
+{
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";   
 };
+
+
+var animatePoints = function(points) 
+{  forEach(points, revealPoint);  };
+
 
 window.onload = function()
 {
