3148c1ffe3a0b374280a32422ca62e75bb56d077
winber1 <winber@cox.net>
2015-10-11 23:14:58 -0400

    checkpoint 29 complete - JQuery: Landing Page

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,34 +1,30 @@
-var pointsArray = document.getElementsByClassName('point');
- 
-var revealPoint = function(point) 
-{
-    point.style.opacity = 1;
-    point.style.transform = "scaleX(1) translateY(0)";
-    point.style.msTransform = "scaleX(1) translateY(0)";
-    point.style.WebkitTransform = "scaleX(1) translateY(0)";   
+var animatePoints = function() {
+    var revealPoint = function() {
+         // #7
+         $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
+    };
+    $.each($('.point'), revealPoint);
 };
 
-
-var animatePoints = function(points) 
-{  forEach(points, revealPoint);  };
-
-
-window.onload = function()
+$(window).load(function() 
 {
      // Automatically animate the points on a tall screen where scrolling can't trigger the animation
-     if (window.innerHeight > 950) 
-     { animatePoints(pointsArray); }
+    if ($(window).height() > 950) {
+         animatePoints();
+    }
     
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+
     
-    window.addEventListener
-    ('scroll', function(event) 
-            { 
+    $(window).scroll(function(event) 
+        { 
                 console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels"); 
-                if (document.body.scrollTop >= scrollDistance) 
-                { animatePoints(pointsArray); }
+                if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
             }
-    );
-}
+        });
+});
 
